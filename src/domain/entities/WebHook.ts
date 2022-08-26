44490830f9d45db2f0a53ffbc1641f8132ac7aad
Felipe Braga <contato@fbsis.com>
2022-08-26 00:43:37 -0300

    feat: domain webhook

---
+++ src/domain/entities/WebHook.ts
@@ -0,0 +1,33 @@
+import {
+  WebHookAction,
+  WebHookId, WebHookInstitutionId, WebHookMethod, WebHookName, WebHookSecret, WebHookStatus, WebHookTimeout, WebHookUrl
+} from '@/domain/valueObjects'
+
+export class WebHook {
+  constructor (
+    private readonly id: WebHookId,
+    private readonly institutionId: WebHookInstitutionId,
+    private readonly action: WebHookAction,
+    private readonly name: WebHookName,
+    private readonly method: WebHookMethod,
+    private readonly url: WebHookUrl,
+    private readonly secret: WebHookSecret,
+    private readonly timeout: WebHookTimeout,
+    private readonly status: WebHookStatus
+
+  ) {}
+
+  toJson (): object {
+    return {
+      id: this.id.toNumber(),
+      institutionId: this.institutionId.toNumber(),
+      action: this.action.toString,
+      name: this.name.toString(),
+      method: this.method.toString(),
+      url: this.url.toString(),
+      secret: this.secret.toString(),
+      timeout: this.timeout.toNumber(),
+      status: this.status.toBoolean()
+    }
+  }
+}
