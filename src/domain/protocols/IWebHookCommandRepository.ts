990ba49d4e8680e7391e4015ce919accd1bda3e0
Felipe Braga <contato@fbsis.com>
2022-08-31 09:12:17 -0300

    feat: webhook command to update

--- src/domain/protocols/IWebHookCommandRepository.ts
+++ src/domain/protocols/IWebHookCommandRepository.ts
@@ -2,4 +2,5 @@ import { WebHook } from '@/domain/entities'
 
 export interface IWebHookCommandRepository {
   create: (webhook: WebHook) => Promise<void>
+  update: (webhook: WebHook) => Promise<void>
 }
