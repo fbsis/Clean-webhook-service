dd922935ec4814eaafe6a49a60019fc3e24a4ba6
Felipe Braga <contato@fbsis.com>
2022-09-01 15:25:23 -0300

    feat: webhook id validation

--- src/domain/valueObjects/WebHookId.ts
+++ src/domain/valueObjects/WebHookId.ts
@@ -1,7 +1,18 @@
-export class WebHookId {
-  constructor (private readonly value: string) {}
+import { DomainException } from '../exceptions'
 
-  toString (): string {
+export class WebHookId {
+  constructor (private readonly value: string) {
+    if (value === '') return
+    if (!value || !this.isValid(value)) throw new DomainException('InvalidWebhookId')
+  }
+
+  toString (): string { // verificar se o sha está até 512 chars ao criar o hash
     return this.value
+  }
+
+  isValid (value: string): boolean {
+    const regExp = /^[a-f0-9]{32}$/g
+    const pattern = new RegExp(regExp)
+    return pattern.test(value)
   }
 }
