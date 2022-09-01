ee2d0170ac5791476d793c08a2b50354b33a890b
Felipe Braga <contato@fbsis.com>
2022-09-01 14:52:14 -0300

    feat: webhook command repository protocols

--- src/domain/protocols/IWebHookCommandRepository.ts
+++ src/domain/protocols/IWebHookCommandRepository.ts
@@ -1,6 +1,8 @@
 import { WebHook } from '@/domain/entities'
+import { WebHookId } from '../valueObjects'
 
 export interface IWebHookCommandRepository {
   create: (webhook: WebHook) => Promise<void>
   update: (webhook: WebHook) => Promise<void>
+  delete: (id: WebHookId) => Promise<void>
 }
