1da2236a11becd19306163fd8fe3782802fac883
Felipe Braga <contato@fbsis.com>
2022-09-07 18:42:06 -0300

    feat: infra implement webhook query repository

--- src/infra/repository/WebhookQueryRepository.ts
+++ src/infra/repository/WebhookQueryRepository.ts
@@ -1,7 +1,8 @@
 import { WebHook } from '@/domain/entities'
 import { IWebHookQueryRepository } from '@/domain/protocols'
 import { WebHookId, WebHookInstitutionId, WebHookAction, WebHookName, WebHookMethod, WebHookUrl, WebHookSecret, WebHookTimeout, WebHookStatus } from '@/domain/valueObjects'
-import { DatabaseService } from '@/infra/repository'
+import { DatabaseService, HttpService } from '@/infra/repository'
+import { InfraNotFoundException } from '../exception'
 
 export class WebhookQueryRepository implements IWebHookQueryRepository {
   async getAll (): Promise<WebHook[]> {
@@ -19,5 +20,27 @@ export class WebhookQueryRepository implements IWebHookQueryRepository {
       new WebHookTimeout(webhook.timeout),
       new WebHookStatus(webhook.status)
     ))
+  }
+
+  async action (institutionId: WebHookInstitutionId, action: WebHookAction): Promise<object> {
+    const database = new DatabaseService()
+    const actionToDo = await database.findOneBy({ institutionId: institutionId.toNumber(), action: action.toString() })
+    if (!actionToDo) throw new InfraNotFoundException('doNotExistsInstitutionIdOrAction')
+
+    const webhook = new WebHook(
+      new WebHookId(actionToDo.id),
+      new WebHookInstitutionId(actionToDo.institutionId),
+      new WebHookAction(actionToDo.action),
+      new WebHookName(actionToDo.name),
+      new WebHookMethod(actionToDo.method),
+      new WebHookUrl(actionToDo.url),
+      new WebHookSecret(actionToDo.secret),
+      new WebHookTimeout(actionToDo.timeout),
+      new WebHookStatus(actionToDo.status)
+    )
+    const httpService = new HttpService()
+    const httpResponse = await httpService.call(webhook)
+
+    return httpResponse
   }
 }
--- src/infra/repository/index.ts
+++ src/infra/repository/index.ts
@@ -1,3 +1,4 @@
 export * from './WebhookCommandRepository'
 export * from './DatabaseService'
 export * from './WebhookSchema'
+export * from './HttpService'
